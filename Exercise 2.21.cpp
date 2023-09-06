fd0b286c2a49bdc1228e71f982b8e22888a08657
JROJAS87 <josenrique.rojas@ucsp.edu.pe.com>
2023-09-06 01:40:43 -0500

    Primer Commit

---
+++ Exercise 2.21.cpp
@@ -0,0 +1,19 @@
+// (Displaying Large Letters)
+// Write a program that prints C++ as follows:
+//    CCC   +      +
+//   C      +      +
+//  C     +++++  +++++
+//   C      +      +
+//    CCC   +      +
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "   CCC   +      +" << std::endl;
+    std::cout << "  C      +      +" << std::endl;
+    std::cout << " C     +++++  +++++" << std::endl;
+    std::cout << "  C      +      +" << std::endl;
+    std::cout << "   CCC   +      +" << std::endl;
+    return 0;
+}
